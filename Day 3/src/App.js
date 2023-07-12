9b88c56fe57a95f361efec56a24a23b74c5cae82
HIROSINI <113608961+HIROSINI@users.noreply.github.com>
2023-07-12 10:00:58 +0530

    Add files via upload

---
+++ Day 3/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+import Home from './Home';
+import About from './About';
+import Privacy from './Privacy';
+import Profile from './Profile';
+import Events from './Events';
+
+function App() {
+  return (
+    <>
+    <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/Home" element={<Home/>}></Route>
+        <Route path="/About" element={<About/>}></Route>
+        <Route path="/Privacy" element={<Privacy/>}></Route>
+        <Route path="/Events" element={<Events/>}></Route>
+        <Route path="/Profile" element={<Profile/>}></Route>
+    </Routes>
+  </Router>
+    </div>
+    </>
+  );
+}
+
+export default App;
